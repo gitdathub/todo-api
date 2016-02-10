f84cc323a2d077a40e668bb386b02474fce1136d
Godofgods <bunewsgroup@gmail.com>
2016-02-10 01:49:46 -0500

    refactor APi with underscore

--- server.js
+++ server.js
@@ -1,39 +1,38 @@
-var express = require('express');
-var app = express();
-var PORT = process.env.PORT || 3000;
-var todos = [];
-var todoNextId = 1;
-var bodyParser = require('body-parser');
+var express = require('express'),
+    app = express(),
+    PORT = process.env.PORT || 3000,
+    todos = [],
+    todoNextId = 1,
+    bodyParser = require('body-parser'),
+    _ = require('underscore');
 
 
-app.use(bodyParser.json());
 
-app.get('/', function(req, res){
-    res.send('todo API Root');
-});
+    app.use(bodyParser.json());
 
-app.get('/todos', function(req, res){
-    res.json(todos); 
-});
+    app.get('/', function(req, res){
+        res.send('todo API Root');
+    });
+
+    app.get('/todos', function(req, res){
+        res.json(todos); 
+    });
 
 app.get('/todos/:id', function(req, res){
-    var todoId = parseInt(req.params.id, 10);
-    var match;
-    todos.forEach(function(item){
-       if(todoId === item.id){
-           res.json(item);
-           match = true;
-       }     
-    });
- 
-    if(!match){
+    var todoId = parseInt(req.params.id, 10),
+        match = _.findWhere(todos, {id: todoId});
+    
+    if(match){
+        res.json(match);
+    }else{
         res.status(404).send();
     }
 });
 
 app.post('/todos', function(req, res){
-    var body = req.body;
-    body.id = ++todoNextId;
+    var body = _.pick(req.body, 'description', 'completed');
+    body.description = body.description.trim();
+    body.id = todoNextId++;
     todos.push(body);
     res.json(todos); 
 });
